a91a88a485a89a47cf7ed3d3a90f0897a2a931d6
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-07-17 12:51:09 +0300

    Changed some names so that it would fit with other classes

--- SwitchCase.h
+++ SwitchCase.h
@@ -1,13 +1,12 @@
 #pragma once
 #include <unordered_map>
 #include <functional>
-#include <optional>
 
 template<typename Ty>
 class SwitchCase
 {
 public:
-	SwitchCase& switchOn(const Ty& val, std::function<void()> fn = {})
+	SwitchCase& operator()(const Ty& val, std::function<void()> fn = {})
 	{
 		if (auto it = m_switch_map.find(val); it != m_switch_map.end())
 		{
@@ -18,12 +17,12 @@ public:
 			fn();
 		return *this;
 	}
-	SwitchCase& addCase(const Ty& val, std::function<void()> fn)
+	SwitchCase& add(const Ty& val, std::function<void()> fn)
 	{
 		m_switch_map[val] = fn;
 		return *this;
 	}
-	const size_t caseSize() const
+	const size_t size() const
 	{
 		return m_switch_map.size();
 	}
