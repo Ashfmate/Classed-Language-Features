22ae5baf5fce17bf9667008e17c73572fed2aa8e
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-07-22 10:03:28 +0300

    Testing out the classses

--- Classed Language Features.cpp
+++ Classed Language Features.cpp
@@ -8,16 +8,16 @@ int main()
 	IfElse ie{};
 	std::string res;
 	cs
-		.add("A", [&res]() { std::cout << "Case A has been hit\n"; res = "A"; })
-		.add("B", [&res]() { std::cout << "Case B has been hit\n"; res = "B"; })
-		.add("C", [&res]() { std::cout << "Case C has been hit\n"; res = "C"; })
+		.add("A", [&res]{ std::cout << "Case A has been hit\n"; res = "A"; })
+		.add("B", [&res]{ std::cout << "Case B has been hit\n"; res = "B"; })
+		.add("C", [&res]{ std::cout << "Case C has been hit\n"; res = "C"; })
 		("B")
-		;
+	;
 	ie
-		.add([&res]() { return res == "A"; }, [&res]() { std::cout << "Result is " << res << "\n"; })
-		.add([&res]() { return res == "B"; }, [&res]() { std::cout << "Result is " << res << "\n"; })
-		.add([&res]() { return res == "C"; }, [&res]() { std::cout << "Result is " << res << "\n"; })
-		([]() { std::cout << "result is not A or B or C\n"; });
-		;
+		.add([&res]{ return res == "A"; }, [&res]{ std::cout << "Result is " << res << "\n"; })
+		.add([&res]{ return res == "B"; }, [&res]{ std::cout << "Result is " << res << "\n"; })
+		.add([&res]{ return res == "C"; }, [&res]{ std::cout << "Result is " << res << "\n"; })
+		([] { std::cout << "result is not A or B or C\n"; })
+	;
 	std::cin.get();
 }
